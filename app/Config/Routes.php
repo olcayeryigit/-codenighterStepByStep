047688f02c06e2b6baaf2a77af470cddf322d7f6
olcay <olcayeryigit@hotmail.com>
2025-02-01 17:50:21 +0300

    ilk-yönlendirme-home

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use App\Controllers\PageController;
 
 /**
  * @var RouteCollection $routes
- */
+ */ 
 $routes->get('/',[PageController::class,'index']);
 
 /*Bu satır, root URL (/) için bir yönlendirme tanımlar. GET isteği ile gelen ziyaretçileri, Home sınıfının index metoduna yönlendirir. Eğer kullanıcı tarayıcısında http://localhost:8080/ adresini ziyaret ederse, Home::index metodu çalıştırılır ve genellikle anasayfa sayfası gösterilir.*/
